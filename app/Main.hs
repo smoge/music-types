4c87225bf31b4ff29083c6cb868303358c3077e9
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-06-20 17:39:16 -0400

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -10,3 +10,36 @@ main = do
     let result = fromRight (error "Parsing error") (getRtmProportions "(1 -1 (1 (1 (1 (1 -1 1)) 1)) 1 (1 (1 -1 (1 (1 1 1 1 -1)) 1)) 1)")
     printf (formatRtmProportions 0 result)
 
+{- 
+
+RtmNote 1
+RtmRest 1
+RtmLeaf
+  1
+  RtmNote 1
+  RtmLeaf
+    1
+    RtmNote 1
+    RtmRest 1
+    RtmNote 1
+
+  RtmNote 1
+
+RtmNote 1
+RtmLeaf
+  1
+  RtmNote 1
+  RtmRest 1
+  RtmLeaf
+    1
+    RtmNote 1
+    RtmNote 1
+    RtmNote 1
+    RtmNote 1
+    RtmRest 1
+
+  RtmNote 1
+
+RtmNote 1
+
+-}
