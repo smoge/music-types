cc6fbb104889336328e2e0c0e1c3e473518d71ea
Bernardo Barros <bernardobarros@gmail.com>
2023-07-01 01:09:00 -0400

    barlows indispensability (experimental working in progress) 3

--- src/Barlow.hs
+++ src/Barlow.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+{-# OPTIONS_GHC -Wno-identities #-}
 module Barlow
   ( primeFactors,
     indispensability
@@ -6,7 +8,6 @@ where
 
 import           Data.List
 
--- Function to calculate prime factors of a number
 primeFactors :: Integer -> [Integer]
 primeFactors n = go n 2 []
   where
@@ -15,37 +16,34 @@ primeFactors n = go n 2 []
       | n `mod` i == 0 = go (n `div` i) i (factors ++ [i])
       | otherwise = go n (i + 1) factors
 
--- Function to calculate wFunc
 wFunc :: Integer -> Integer
 wFunc 0 = 0
 wFunc _ = 1
 
--- Function to calculate basic indispensability
 basicIndispensability :: Integer -> Integer -> Integer
 basicIndispensability pulse prime
-  | prime <= 3 = mod (prime + pulse - 2) prime
+  | prime <= 3 = (prime + pulse - 2) `mod` prime
   | otherwise =
-    (q + wFunc (div q (div prime 4))) * wFunc (prime - pulse - 1)
-      + div prime 4 * (1 - wFunc (prime - pulse - 1))
+      (q + wFunc (q `div` (prime `div` 4))) * wFunc (prime - pulse - 1)
+      + (prime `div` 4) * (1 - wFunc (prime - pulse - 1))
   where
     newPulse = pulse - 1 + wFunc (prime - pulse)
     factors = reverse $ primeFactors (prime - 1)
     q = indispensability newPulse factors
 
--- Function to calculate indispensability
 indispensability :: Integer -> [Integer] -> Integer
 indispensability pulse primes =
   sum
     [ mult * basic
-      | r <- [0 .. z - 1],
-        let bot = product $ take (r + 1) (drop (z + 1 - r) extendedPrimes),
-        let mult = product $ take (z - r) extendedPrimes,
-        let modulo = extendedPrimes !! (z - r),
-        let temp = fromIntegral ((pulse - 2) `mod` top),
-        let temp' = 1 + floor (temp / fromIntegral bot),
-        let temp'' = fromIntegral (mod temp' modulo),
-        let temp''' = 1 + temp'',
-        let basic = basicIndispensability temp''' modulo
+    | r <- [0 .. z - 1]
+    , let bot = product $ take (r + 1) (drop (z + 1 - r) extendedPrimes)
+    , let mult = product $ take (z - r) extendedPrimes
+    , let modulo = extendedPrimes !! (z - r)
+    , let temp = fromIntegral ((pulse - 2) `mod` top)
+    , let temp' = 1 + floor (temp / fromIntegral bot)
+    , let temp'' = fromIntegral (temp' `mod` modulo)
+    , let temp''' = 1 + temp''
+    , let basic = basicIndispensability temp''' modulo
     ]
   where
     extendedPrimes = [1] ++ primes ++ [1]
@@ -54,7 +52,6 @@ indispensability pulse primes =
 
 main :: IO ()
 main = do
-  -- Test cases
   print $ indispensability 1 [2, 3, 2, 2, 2]
   print $ indispensability 2 [2, 3, 2, 2, 2]
   print $ indispensability 3 [2, 3, 2, 2, 2]
