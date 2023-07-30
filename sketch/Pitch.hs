decb917b27c6c0e0845a2b87caf8364dd0dcc945
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-07-30 03:41:48 -0400

    VectorSpace+Lens for new Pitch implementation

--- sketch/Pitch.hs
+++ sketch/Pitch.hs
@@ -1,11 +1,41 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TemplateHaskell #-}
 
+import Control.Lens
 import Data.Fixed
 import Data.Function (on)
 import Data.List (minimumBy)
 import Data.Ratio
 import Data.Kind (Type)
+
+data NoteName = C | D | E | F | G | A | B deriving (Show, Eq, Enum, Bounded, Ord)
+data PitchSpace = PitchSpace {
+    _pitch :: Rational,
+    _note :: Note,
+    _octave :: Int
+} deriving (Eq, Show)
+
+data Note = Note {
+    _noteName :: NoteName,
+    _accidental :: Accidental
+} deriving (Eq, Show)
+
+data Accidental = Accidental {
+    _accidentalName :: String,
+    _shift :: Rational
+} deriving (Eq, Show)
+
+makeLenses ''PitchSpace
+makeLenses ''Note
+makeLenses ''Accidental
+makeFields ''PitchSpace  -- generates lenses for nested fields 
+
+-- update the Note based on the modified pitch
+updateNote :: PitchSpace -> PitchSpace
+updateNote ps = ps & note .~ Note noteName acc where
+    (noteName, acc) = getClosestNoteAndAccidental (ps ^. pitch)
 
 class AffineSpace p where
   type Diff p :: Type
@@ -25,23 +55,6 @@ instance VectorSpace Interval where
   type Scalar Interval = Rational
   (*^) s (Interval i) = Interval (s * i)
 
-data NoteName = C | D | E | F | G | A | B deriving (Show, Eq, Enum, Bounded, Ord)
-
-data Note = Note {
-    noteName :: NoteName,
-    accidental :: Accidental
-} deriving (Eq, Show)
-
-data Accidental = Accidental {
-    accidentalName :: String,
-    shift :: Rational
-} deriving (Eq, Show)
-
-data PitchSpace = PitchSpace {
-    pitch :: Rational,
-    note :: Note,
-    octave :: Int
-} deriving (Eq, Show)
 
 instance AffineSpace PitchSpace where
   type Diff PitchSpace = Interval
@@ -53,6 +66,8 @@ instance AffineSpace PitchSpace where
     in PitchSpace r' (Note noteName accidental) octave'
     
   (PitchSpace r1 _ _) .-. (PitchSpace r2 _ _) = Interval (r1 - r2)
+
+  
 
 type RationalNote = (NoteName, Rational)
 
@@ -168,3 +183,83 @@ p2 .+^ (-i)
 -- Interval (9 % 1)
 
   -}
+
+
+
+
+  {- 
+  
+  import Data.Ratio
+
+p1 :: PitchSpace
+p1 = rationalToPitchSpace (1 % 1)
+
+p2 :: PitchSpace
+p2 = rationalToPitchSpace (5.5)
+
+-- Accessing fields using lenses
+p1Pitch :: Rational
+p1Pitch = p1 ^. pitch  -- Equivalent to 'pitch p1'
+
+p2NoteName :: NoteName
+p2NoteName = p2 ^. note . noteName  -- Equivalent to 'noteName (note p2)'
+
+p1Octave :: Int
+p1Octave = p1 ^. octave  -- Equivalent to 'octave p1'
+
+-- Modifying fields using lenses
+modifiedP1 :: PitchSpace
+modifiedP1 = p1 & pitch %~ (+ (1 % 2))  -- Add 1/2 to the pitch of p1
+
+modifiedP2 :: PitchSpace
+modifiedP2 = p2 & (note . accidental . shift) %~ (* 2)  -- Multiply the shift of the accidental of the note in p2 by 2
+
+-- Combining lenses
+accidentalNameInP2 :: String
+accidentalNameInP2 = p2 ^. note . accidental . accidentalName  -- Equivalent to 'accidentalName (note p2)'
+
+-- Modifying fields conditionally using lenses
+adjustedPitchSpace :: Rational -> PitchSpace -> PitchSpace
+adjustedPitchSpace threshold ps =
+    ps & pitch %~ (\p -> if p > threshold then p - 1 else p)  -- If pitch > threshold, subtract 1 from the pitch
+
+adjustedP1 :: PitchSpace
+adjustedP1 = adjustedPitchSpace (3 % 2) p1
+
+adjustedP2 :: PitchSpace
+adjustedP2 = adjustedPitchSpace (5 % 1) p2
+
+
+ghci> p1 = rationalToPitchSpace (1 % 1)
+ghci> p2 = rationalToPitchSpace (5.5)
+ghci> p1Pitch = p1 ^. pitch
+ghci> p1Pitch 
+1 % 1
+ghci> p2NoteName = p2 ^. note . noteName
+ghci> p2
+p2          p2NoteName
+ghci> p2NoteName 
+F
+ghci> modifiedP1 = updateNote $ p1 & pitch %~ (+ (1 % 2))
+ghci> modifiedP1 
+PitchSpace {_pitch = 3 % 2, _note = Note {_noteName = D, _accidental = Accidental {_accidentalName = "qf", _shift = (-1) % 2}}, _octave = 0}
+ghci> modifiedP2 = updateNote $ p2 & pitch %~ (+ (13 % 2))
+ghci> modifiedP2
+PitchSpace {_pitch = 12 % 1, _note = Note {_noteName = C, _accidental = Accidental {_accidentalName = "", _shift = 0 % 1}}, _octave = 0}
+ghci> 
+
+
+ghci> modifiedP1 = updateNote $ p1 & pitch %~ (+ (1 % 2))
+ghci> modifiedP1 
+PitchSpace {_pitch = 3 % 2, _note = Note {_noteName = D, _accidental = Accidental {_accidentalName = "qf", _shift = (-1) % 2}}, _octave = 0}
+ghci> modifiedP2 = updateNote $ p2 & pitch %~ (+ (13 % 2))
+ghci> modifiedP2
+PitchSpace {_pitch = 12 % 1, _note = Note {_noteName = C, _accidental = Accidental {_accidentalName = "", _shift = 0 % 1}}, _octave = 0}
+ghci> modifiedP3 = updateNote $ p2 & pitch %~ (+ (15 % 2))
+ghci> modifiedP3
+PitchSpace {_pitch = 13 % 1, _note = Note {_noteName = C, _accidental = Accidental {_accidentalName = "s", _shift = 1 % 1}}, _octave = 0}
+ghci> modifiedP4 = updateNote $ p2 & pitch %~ (+ (15 % 1))
+ghci> modifiedP4
+PitchSpace {_pitch = 41 % 2, _note = Note {_noteName = A, _accidental = Accidental {_accidentalName = "qf", _shift = (-1) % 2}}, _octave = 0}
+ghci> 
+   -}
