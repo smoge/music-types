ad61e34e9b0389f0a22553101d0f083fc8133737
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-08-26 04:16:34 -0400

    Duration and Rq fix more than one dur to a rq

--- src/Time/Duration.hs
+++ src/Time/Duration.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
 
 module Time.Duration where
 
@@ -35,6 +37,15 @@ dotMultiplier dotCount = n % d
 durationToRq :: Duration -> Rq
 durationToRq d = (1 % (d ^. division)) * dotMultiplier (d ^. dots) * (d ^. multiplier)
 
+durationToRq' :: Duration -> Rational
+durationToRq' d = a * b * c
+  where
+    a = 1 % (view division d) :: Rational 
+    b = dotMultiplier (view dots d) :: Rational
+    c = view multiplier d :: Rational
+
+      
+
 compareDurations :: Duration -> Duration -> Ordering
 compareDurations = comparing durationToRq
 
@@ -55,6 +66,29 @@ rqToDuration rq
           closestDuration = minimumBy (comparing $ \d -> abs (durationToRq d - rq)) validDurations
        in if durationToRq closestDuration == rq then Just closestDuration else Nothing
 
+rqToDuration' :: Rq -> Maybe Duration
+rqToDuration' rq
+  | rq <= 0 = Nothing
+  | otherwise =
+      let divisions = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+          dots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+          multipliers = [1, 2 / 3, 4 / 5]
+          validDurations = [Duration d dt m | d <- divisions, dt <- dots, m <- multipliers]
+          closestDuration = minimumBy (comparing $ \d -> abs (durationToRq d - rq)) validDurations
+       in if durationToRq closestDuration == rq then Just closestDuration else Nothing
+
+rqToDuration'' :: Rq -> [Duration]
+rqToDuration'' rq
+  | rq <= 0 = []
+  | otherwise =
+      let divisions = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+          dots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+          multipliers = [1, 2 / 3, 4 / 5]
+          validDurations = [Duration d dt m | d <- divisions, dt <- dots, m <- multipliers]
+          matchingDurations = filter (\d -> durationToRq d == rq) validDurations
+       in matchingDurations
+
+
 durationToLilypondType :: Duration -> String
 durationToLilypondType dur =
   let divStr =
@@ -71,17 +105,44 @@ accessDivision = view division
 ---------------------------------------------------------------------------- !!
 
 
+-- instance Arbitrary Duration where
+--   arbitrary = do
+--     div <- Test.QuickCheck.elements [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+--     dt <- choose (0, 12)
+--     return Duration {_division = div, _dots = dt, _multiplier = (1 % 1)}
+
+-- instance Arbitrary Duration where
+--   arbitrary = do
+--     div <- Test.QuickCheck.elements [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+--     dt <- choose (0, 2)
+--     mult <- Test.QuickCheck.elements [1, 2 / 3, 4 / 5]
+--     return Duration {_division = div, _dots = dt, _multiplier = mult}
+
+-- -- prop_durationToRq :: Duration -> Bool
+-- -- prop_durationToRq d = (rqToDuration . durationToRq) d == Just d
+
+-- prop_rqToDuration' :: Duration -> Bool
+-- prop_rqToDuration' d = (rqToDuration' . durationToRq) d == Just d
+
+-- runTests :: IO ()
+-- runTests = verboseCheck prop_rqToDuration'
+
+
 instance Arbitrary Duration where
   arbitrary = do
-    div <- Test.QuickCheck.elements [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
-    dt <- choose (0, 12)
-    return Duration {_division = div, _dots = dt, _multiplier = (1 % 1)}
+    div <- Test.QuickCheck.elements [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+    dt <- choose (0, 2)
+    mult <- Test.QuickCheck.elements [1, 2 / 3, 4 / 5]
+    return Duration {_division = div, _dots = dt, _multiplier = mult}
 
-prop_durationToRq :: Duration -> Bool
-prop_durationToRq d = (rqToDuration . durationToRq) d == Just d
+prop_rqToDuration' :: Duration -> Bool
+prop_rqToDuration' d =
+  let rq = durationToRq d
+      durations = rqToDuration'' rq
+   in d `elem` durations
 
 runTests :: IO ()
-runTests = quickCheckWith (stdArgs {maxSuccess = 1000}) prop_durationToRq
+runTests = verboseCheck prop_rqToDuration'
 
 ---------------------------------------------------------------------------- !!
 -- ~ End QuickCheck 
