04d221a06c4e78546571d0f214a99c27578e5df3
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-08-29 07:17:25 -0400

    optimizations Time.Duration

--- src/Time/Duration.hs
+++ src/Time/Duration.hs
@@ -1,31 +1,51 @@
+
+-- | Module defines a `Duration` data type and provides functions for converting Durations to Rq and vice versa.
+--
+-- The `Duration` data type represents a duration, consisting of three fields: `_division`, `_dots`, and `_multiplier`.
+--
+-- The module includes the following functions:
+--   - `durationToRq`: Converts a duration to a rational number.
+--   - `rqToDuration`: Converts a rational number to a list of durations.
+--
+-- Example usage:
+--   >>> duration = Duration 8 1 (4 % 5)
+--   >>> durationToRq duration
+--   -- Output: 3 % 20
+--
+--   >>> rq = 7 % 12
+--   >>> rqToDuration rq
+--   -- Output: [Duration {_division = 2, _dots = 1, _multiplier = 7 % 9},Duration {_division = 2, _dots = 2, _multiplier = 2 % 3}]
+--
+-- The module also includes QuickCheck tests and helper functions for generating and manipulating durations and rational numbers.
+
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Time.Duration
   ( Duration (..),
     Rq,
-    Division,
-    Dots,
     durationToRq,
     rqToDuration,
   )
 where
 
-import Control.Lens (makeLenses, view)
+import Control.Lens (makeLenses)
 import Data.Ord (comparing)
 import Data.Ratio ((%))
 import qualified Data.Set as Set
 import Test.QuickCheck
 
-type Division = Integer
+type Division = Int
 
 type Dots = Int
 
 type Rq = Rational
 
 data Duration = Duration
-  { _division :: Division,
-    _dots :: Dots,
-    _multiplier :: Rational
+  { _division :: !Division,
+    _dots :: !Dots,
+    _multiplier :: !Rational
   }
   deriving (Eq, Show)
 
@@ -35,80 +55,87 @@ class HasDuration a where
   _duration :: a -> Duration
   _rq :: a -> Rq
   _rq a = durationToRq (_duration a)
-
+  _duration _ = Duration 1 0 1 -- default implementation
+ 
 dotMultiplier :: Dots -> Rational
-dotMultiplier 0 = 1
-dotMultiplier 1 = 3 / 2
-dotMultiplier 2 = 7 / 4
-dotMultiplier 3 = 15 / 8
-dotMultiplier 4 = 31 / 16
-dotMultiplier 5 = 63 / 32
-dotMultiplier 6 = 127 / 64
-dotMultiplier _ = error "Invalid number of dots"
+dotMultiplier d = dotArray !! d
+  where
+    dotArray = [1, 3 / 2, 7 / 4, 15 / 8, 31 / 16, 63 / 32, 127 / 64]
 
-generateDivisions :: Int -> [Int]
+
+
+
+generateDivisions :: Int -> [Division]
 generateDivisions n = [2^x | x <- [0..n]]
 
 divisions :: [Division]
-divisions = map fromIntegral (generateDivisions 9)
+divisions = generateDivisions 9
 
-durationToRq :: Duration -> Rational
-durationToRq (Duration div dots mult) =
-  (1 % div) * dotMultiplier dots * mult
+
 
 rationalNumbers :: [Rational]
-rationalNumbers =
-  [ 2 % 3,
-    3 % 4,
-    3 % 5,
-    4 % 5,
-    4 % 7,
-    4 % 9,
-    5 % 6,
-    5 % 7,
-    5 % 8,
-    5 % 9,
-    6 % 7,
-    6 % 11,
-    7 % 8,
-    7 % 9,
-    7 % 10,
-    7 % 11,
-    8 % 9,
-    8 % 11,
-    9 % 10,
-    9 % 11,
-    10 % 11
-  ]
+rationalNumbers = [2 % 3, 3 % 4, 3 % 5, 4 % 5, 4 % 7, 4 % 9, 5 % 6, 5 % 7, 5 % 8, 5 % 9, 6 % 7, 6 % 11, 7 % 8, 7 % 9, 7 % 10, 7 % 11, 8 % 9, 8 % 11, 9 % 10, 9 % 11, 10 % 11]
+
+dotsSet :: Set.Set Dots
+dotsSet = Set.fromList dotsList
+
+multipliersSet :: Set.Set Rq
+multipliersSet = Set.fromList rationalNumbers
+
+-- rationalNumbers :: [Rational]
+-- rationalNumbers = [ x % y | x <- [2..10], y <- [3..11], x < y ]
 
 dotsList :: [Dots]
 dotsList = [0 .. 6]
 
 compareDurations :: Duration -> Duration -> Ordering
-compareDurations = comparing durationToRq
+compareDurations = Data.Ord.comparing durationToRq
 
 instance Ord Duration where
+  compare :: Duration -> Duration -> Ordering
   compare = compareDurations
 
-isMultiplierIdentity :: Duration -> Bool
-isMultiplierIdentity = (1 ==) . view multiplier
+
+
+durationToRq :: Duration -> Rational
+durationToRq (Duration div dots mult) =
+  (1 % fromIntegral div) * dotMultiplier dots * mult
 
 rqToDuration :: Rq -> [Duration]
 rqToDuration rq
   | rq <= 0 = []
-  | otherwise = concatMap potentialDurations divisions
+  | otherwise = foldMap potentialDurations divisions
   where
-    multipliersSet = Set.fromList rationalNumbers
-
     potentialDurations :: Division -> [Duration]
-    potentialDurations d = concatMap (potentialDurationsForDot d) dotsList
+    potentialDurations d = foldMap (potentialDurationsForDot d) dotsList
 
     potentialDurationsForDot :: Division -> Dots -> [Duration]
     potentialDurationsForDot d dt =
-      let potentialMultiplier = rq / ((1 % d) * dotMultiplier dt)
+      let !reciprocalDiv = 1 % fromIntegral d
+          !dotMult = dotMultiplier dt
+          !divisor = reciprocalDiv * dotMult
+          potentialMultiplier = rq / divisor
        in [Duration d dt potentialMultiplier | Set.member potentialMultiplier multipliersSet]
 
--- QuickCheck
+
+-- rqToDuration :: Rq -> [Duration]
+-- rqToDuration rq
+--   | rq <= 0 = []
+--   | otherwise = concatMap potentialDurations divisions
+--   where
+--     multipliersSet = Set.fromList rationalNumbers
+
+--     potentialDurations :: Division -> [Duration]
+--     potentialDurations d = concatMap (potentialDurationsForDot d) dotsList
+
+--     potentialDurationsForDot :: Division -> Dots -> [Duration]
+--     potentialDurationsForDot d dt =
+--       let potentialMultiplier = rq / ((1 % d) * dotMultiplier dt)
+--        in [Duration d dt potentialMultiplier | Set.member potentialMultiplier multipliersSet]
+
+
+
+-- !QuickCheck
 
 instance Arbitrary Duration where
   arbitrary = do
@@ -145,4 +172,3 @@ durationsForRq rq = filter (\d -> durationToRq d == rq) allDurations
 
 countDurationsForRq :: Rq -> Int
 countDurationsForRq rq = length $ durationsForRq rq
-
